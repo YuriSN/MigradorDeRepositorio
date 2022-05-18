580faed387fd030da1c82943e493422f481b5721
Yuri Silva <yuri.sele@outlook.com>
2022-05-18 13:02:35 -0300

    Adicionado método cópia de pasta.

--- Migração/Form1.Designer.cs
+++ Migração/Form1.Designer.cs
@@ -35,9 +35,11 @@
             this.txtDestino = new DevExpress.XtraEditors.TextEdit();
             this.cmdCopiar = new DevExpress.XtraEditors.SimpleButton();
             this.cmdMover = new DevExpress.XtraEditors.SimpleButton();
-            this.cmdRenomear = new DevExpress.XtraEditors.SimpleButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.txtOrigem.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtDestino.Properties)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblOrigem
@@ -91,7 +93,7 @@
             this.cmdCopiar.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cmdCopiar.Appearance.Font = new System.Drawing.Font("Bahnschrift Light Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdCopiar.Appearance.Options.UseFont = true;
-            this.cmdCopiar.Location = new System.Drawing.Point(225, 95);
+            this.cmdCopiar.Location = new System.Drawing.Point(261, 95);
             this.cmdCopiar.Name = "cmdCopiar";
             this.cmdCopiar.Size = new System.Drawing.Size(81, 26);
             this.cmdCopiar.TabIndex = 2;
@@ -103,24 +105,30 @@
             this.cmdMover.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cmdMover.Appearance.Font = new System.Drawing.Font("Bahnschrift Light Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdMover.Appearance.Options.UseFont = true;
-            this.cmdMover.Location = new System.Drawing.Point(312, 95);
+            this.cmdMover.Location = new System.Drawing.Point(364, 95);
             this.cmdMover.Name = "cmdMover";
             this.cmdMover.Size = new System.Drawing.Size(81, 26);
             this.cmdMover.TabIndex = 2;
             this.cmdMover.Text = "Mover";
             this.cmdMover.Click += new System.EventHandler(this.cmdMover_Click);
             // 
-            // cmdRenomear
+            // statusStrip1
             // 
-            this.cmdRenomear.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.cmdRenomear.Appearance.Font = new System.Drawing.Font("Bahnschrift Light Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmdRenomear.Appearance.Options.UseFont = true;
-            this.cmdRenomear.Location = new System.Drawing.Point(399, 95);
-            this.cmdRenomear.Name = "cmdRenomear";
-            this.cmdRenomear.Size = new System.Drawing.Size(81, 26);
-            this.cmdRenomear.TabIndex = 2;
-            this.cmdRenomear.Text = "Renomear";
-            this.cmdRenomear.Click += new System.EventHandler(this.cmdRenomear_Click);
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(5, 138);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(217, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(200, 17);
             // 
             // Migração
             // 
@@ -128,7 +136,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(702, 162);
-            this.Controls.Add(this.cmdRenomear);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.cmdMover);
             this.Controls.Add(this.cmdCopiar);
             this.Controls.Add(this.txtDestino);
@@ -142,6 +150,8 @@
             this.Load += new System.EventHandler(this.Migração_Load);
             ((System.ComponentModel.ISupportInitialize)(this.txtOrigem.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtDestino.Properties)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,7 +165,8 @@
         private DevExpress.XtraEditors.TextEdit txtDestino;
         private DevExpress.XtraEditors.SimpleButton cmdCopiar;
         private DevExpress.XtraEditors.SimpleButton cmdMover;
-        private DevExpress.XtraEditors.SimpleButton cmdRenomear;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- Migração/Form1.cs
+++ Migração/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
@@ -47,64 +48,129 @@ namespace Migração
 
         }
 
-        
+
         private void cmdCopiar_Click(object sender, EventArgs e)
-        
+
         {
-            try
+            
+            if (txtOrigem.Text.Contains(".pdf") || txtOrigem.Text.Contains(".png") || txtOrigem.Text.Contains(".jpg") || txtOrigem.Text.Contains(".txt") || txtOrigem.Text.Contains(".doc") || txtOrigem.Text.Contains(".docx")
+                 || txtOrigem.Text.Contains(".ptt") || txtOrigem.Text.Contains(".xls") || txtOrigem.Text.Contains(".rar") || txtOrigem.Text.Contains(".zip") || txtOrigem.Text.Contains(".css") || txtOrigem.Text.Contains(".exe")
+                  || txtOrigem.Text.Contains(".js") || txtOrigem.Text.Contains(".svg") || txtOrigem.Text.Contains(".sql") || txtOrigem.Text.Contains(".sln") || txtOrigem.Text.Contains(".resx") || txtOrigem.Text.Contains(".cs"))
             {
-                if (File.Exists(txtDestino.Text))              
+                try
                 {
-                    if (MessageBox.Show("Arquivo já existe! \n Deseja sobrepor?", "Copiar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)                   
+                    if (File.Exists(txtDestino.Text))
                     {
-                        var texto1 = txtOrigem.Text.Split('\\');
-                        var tamanhoTexto = texto1.Length;
-                        var nomeArquivo = texto1[tamanhoTexto - 1];
-                        var duplica = nomeArquivo.Split('.');
-                        var iniciotexto = duplica[0];
-                        var finaltexto = duplica[1];
-
-                        string newPath = "";
-
-                        for (int i = 1; i < 10; i++)
+                        if (MessageBox.Show("Arquivo já existe! \n Deseja sobrepor?", "Copiar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                         {
-                            string str = txtDestino.Text;
+                            var texto1 = txtOrigem.Text.Split('\\');
+                            var tamanhoTexto = texto1.Length;
+                            var nomeArquivo = texto1[tamanhoTexto - 1];
+                            var duplica = nomeArquivo.Split('.');
+                            var iniciotexto = duplica[0];
+                            var finaltexto = duplica[1];
+
+                            string newPath = "";
+
+                            for (int i = 1; i < 10; i++)
+                            {
+                                string str = txtDestino.Text;
 
 
-                            string newName = iniciotexto + "(" + i + ")" + "." + finaltexto;
+                                string newName = iniciotexto + "(" + i + ")" + "." + finaltexto;
 
-                            newPath = str.Replace(nomeArquivo, newName);
+                                newPath = str.Replace(nomeArquivo, newName);
 
-                            if (!File.Exists(newPath))
-                                break;       
+                                if (!File.Exists(newPath))
+                                    break;
+                            }
+
+                            File.Copy(txtOrigem.Text, newPath, true);
+                            MessageBox.Show("Arquivo duplicado com sucesso!");
+
                         }
 
-                        File.Copy(txtOrigem.Text, newPath, true);
-                        MessageBox.Show("Arquivo duplicado com sucesso!");
-                        
+                    }
+                    else
+                    {
+                        File.Copy(txtOrigem.Text, txtDestino.Text);
+                        MessageBox.Show("Arquivo cópiado com sucesso!");
+
+                        txtDestino.Text = "";
+                        ActiveControl = txtOrigem;
                     }
 
                 }
-                else {
-                    File.Copy(txtOrigem.Text, txtDestino.Text);
-                    MessageBox.Show("Arquivo cópiado com sucesso!");
-                    
-                    txtDestino.Text = "";
-                    ActiveControl = txtOrigem;
+
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Falha: " + ex.Message);
                 }
-                
             }
+            else
+                CopiarPasta(txtOrigem.Text, txtDestino.Text, true);
 
-            
-            catch (Exception ex)
-            {
-                MessageBox.Show("Falha: " + ex.Message);
-            }
-
-            
-            
         }
 
+         
+
+        //Criando Método copiar pasta:
+        private void CopiarPasta(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            { 
+                var dir = new DirectoryInfo(sourceDirName);
+                var dirs = dir.GetDirectories();
+
+                // If the source directory does not exist, throw an exception.
+                if (!dir.Exists)
+                {
+                    throw new DirectoryNotFoundException(
+                        "Source directory does not exist or could not be found: "
+                        + sourceDirName);
+                }
+
+                // If the destination directory does not exist, create it.
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                }
+
+
+                // Get the file contents of the directory to copy.
+                var files = dir.GetFiles();
+
+                foreach (var file in files)
+                {
+                    toolStripStatusLabel1.Text = Convert.ToString(file);
+                    statusStrip1.Refresh();
+                    // Create the path to the new copy of the file.
+                    var temppath = Path.Combine(destDirName, file.Name);
+                    Thread.Sleep(5);
+
+                    // Copy the file.
+                    file.CopyTo(temppath, true);
+                }
+
+                // If copySubDirs is true, copy the subdirectories.
+                if (!copySubDirs) return;
+
+                foreach (var subdir in dirs)
+                {
+                    toolStripStatusLabel1.Text = Convert.ToString(subdir);
+                    statusStrip1.Refresh();
+                    // Create the subdirectory.
+                    var temppath = Path.Combine(destDirName, subdir.Name);
+                    Thread.Sleep(5);
+
+                    // Copy the subdirectories.
+                    CopiarPasta(subdir.FullName, temppath, copySubDirs);
+                }
+                toolStripStatusLabel1.Text = "Pronto " + dirs.Count() + " subpastas copiadas!";
+            }
+
+            
+        }
 
         private void cmdMover_Click(object sender, EventArgs e)
         {
@@ -119,10 +185,6 @@ namespace Migração
             }
         }
 
-        private void cmdRenomear_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void txtDestino_DoubleClick(object sender, EventArgs e)
         {
@@ -144,14 +206,19 @@ namespace Migração
 
         private void txtOrigem_DoubleClick(object sender, EventArgs e)
         {
-            //Caixa de diálogo:
-            using (var openFile = new OpenFileDialog())
-            {
 
-                if (openFile.ShowDialog() == DialogResult.OK)
-                    txtOrigem.Text = openFile.FileName;//Caminho da pasta selecionada.
+            //Caixa de diálogo:
+            using (var openFolder = new FolderBrowserDialog())
+            {
+                if (openFolder.ShowDialog() == DialogResult.OK)
+                    txtOrigem.Text = openFolder.SelectedPath;//Caminho da pasta selecionado
             }
 
+            using (var openFile = new OpenFileDialog())
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                    txtOrigem.Text = openFile.FileName;//Caminho do arquivo selecionado.
+            }
         }
     }
 }
